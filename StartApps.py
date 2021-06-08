0ff06aa0011aab9c4dc762ea3efeb94601084790
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2021-06-07 23:12:43 -0300

    teste

--- StartApps.py
+++ StartApps.py
@@ -5,6 +5,10 @@ connect('weblogic','weblogic123','t3://127.0.0.1:8001')
 print("*** Connected *****")
 
 start('config_server')
+
+stopApplication('plato-discovery-service-5.3.0')
+startApplication('plato-discovery-service-5.3.0')
+
 start('ac_server')
 start('cs_server')
 start('ic_server')
